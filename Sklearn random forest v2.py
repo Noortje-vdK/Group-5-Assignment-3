e92066cdfcaebf0688d18ca1347bb0b2bb73a201
Cappon <h.l.cappon@student.tue.nl>
2024-12-16 16:28:24 +0100

    Add saved changes

---
+++ Sklearn random forest v2.py
@@ -0,0 +1,9 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+random_forest = RandomForestClassifier(n_estimators=5, criterion='gini', max_depth=6, min_samples_split=2, min_samples_leaf=1, max_features = 'sqrt')
+random_forest.fit(X_train, Y_train)
+y_pred = random_forest.predict(X_test)
+accuracy = accuracy_score(Y_test, y_pred)
+print("Accuracy:", accuracy)
