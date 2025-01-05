e0b14fcda017b7fc371467727044a2713ec6fad7
Koolwijk <n.a.v.d.koolwijk@student.tue.nl>
2025-01-05 21:24:27 +0100

    fixed issue

--- functions.py
+++ functions.py
@@ -8,15 +8,14 @@ from sklearn.metrics import accuracy_score, balanced_accuracy_score
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
 
 def data_processing(filename, testing = True):
-    """Reads in data from file, removes rows with missing data and duplicate rows, and if the data is training data, it removes non-binary
-    target values, if it is new data it removes the column with unique ID to be used for prediction."""
+    """Reads in data from file, removes rows with missing data and duplicate rows, and if the data is training data, 
+    it removes non-binary target values."""
     data = pd.read_csv(filename)
-    data.drop_duplicates()
-    data.dropna()
+    data.drop_duplicates(inplace=True)
+    data.dropna(inplace=True)
     if testing:
-        return data.drop(data[(data['target_feature'] != 0) & (data['target_feature'] != 1)].index) # remove row if target feature is not binary
-    else:
-        return data.drop(columns=["Unique_ID"]) # drop unique ID for easier handling
+        data = data.drop(data[(data['target_feature'] != 0) & (data['target_feature'] != 1)].index) # remove row if target feature is not binary
+    return data
 
 def getMolDescriptors(mol, missingVal=None):
     """For given molecule, calculate all molecular descriptors and returns it in a dictionary."""
@@ -31,7 +30,7 @@ def getMolDescriptors(mol, missingVal=None):
     return descriptors
 
 def calculate_descriptors(df):
-    """For molecules in SMILEs format, convert it to a RDkit molecule and calculate all descriptors for each molcule in dataframe."""
+    """For molecules in SMILES format, convert it to a RDkit molecule and calculate all descriptors for each molcule in dataframe."""
     all_descriptors = []
     for index, row in df.iterrows():
         smile_molc = row["SMILES_canonical"]
@@ -45,7 +44,8 @@ def calculate_descriptors(df):
     return descriptors_df
 
 def remove_correlated_variables(data, threshold):
-    """From data that only has numerical or binary columns, remove columns with variables that are highly correlated: that have a correlation value above the threshold"""
+    """From data that only has numerical or binary columns, remove columns with variables that are highly correlated: 
+    that have a correlation value above the threshold."""
     corr_matrix = data.drop(columns=["target_feature"]).corr()
     upper_triangle = corr_matrix.where(np.triu(np.ones(corr_matrix.shape, dtype=bool), k=1)) # look only at upper triangle without diagonal to prevent removing both variables if correlated
     to_drop = [col for col in upper_triangle.columns if any(upper_triangle[col] > threshold)] 
@@ -58,7 +58,6 @@ def train_test_sets(X):
     X_train, X_test, y_train, y_test = train_test_split(input, output, test_size=0.2, random_state=42)
     return X_train, X_test, y_train, y_test
 
-#scaler = StandardScaler()
 def scaling(data, scaler, fit = True):
     """Scales the data with given scaler, if scaler needs to be fitted to the data, it does so, 
     if it is new data, it only scales it with the existing scaler."""
@@ -71,9 +70,10 @@ def submission(model, newfile, scaler, testfile="test.csv"):
     """Creates a csv file that predicts the target feature for testfile"""
     new_data = data_processing(testfile, False)
     new_descriptors = calculate_descriptors(new_data)
-    new_descriptors.dropna()
+    new_descriptors.fillna(0, inplace= True)
     new_descriptors_scaled = scaling(new_descriptors, scaler, False)
+    new_descriptors_scaled = pd.DataFrame(new_descriptors_scaled, columns=new_descriptors.columns, index=new_descriptors.index) # keep feature names
     predictions = model.predict(new_descriptors_scaled)
     new_data['target_feature'] = predictions
-    new_data['target_feature'] = new_data['target_feature'].astype(str)
-    new_data[['Unique_ID', 'target_feature']].to_csv(newfile, index=False, quoting=1)
+    new_data['target_feature'] = new_data['target_feature'].astype(str) # prevent that zeroes get removed while writing the file
+    new_data[['Unique_ID', 'target_feature']].to_csv(newfile, index=False, quoting=1) # quoting = 1 ensures that dataframa is copied entirely to csv file
--- main.py
+++ main.py
@@ -12,13 +12,23 @@ train_descriptors = calculate_descriptors(training_data)
 final_df = pd.concat([training_data.reset_index(drop=True), train_descriptors], axis=1) # put the descriptors next to existing columns
 scaler = StandardScaler() # used for all functions
 
-def testing_accuracy(dataframe):
+rf_parameters = {'random_state':42, 
+                 'bootstrap': False, 
+                 'class_weight': 'balanced', 
+                 'criterion': 'entropy', 
+                 'max_depth': 160, 
+                 'max_features': 'sqrt', 
+                 'min_samples_leaf': 6, 
+                 'min_samples_split': 6, 
+                 'n_estimators': 1200}
+
+def testing_accuracy(dataframe, rf_params):
     """Trains a random forest model with 80% of the training data to estimate its performance 
     by returning the accuracy and balanced accuracy."""
     X_train, X_test, y_train, y_test = train_test_sets(dataframe)
     X_train_scaled = scaling(X_train, scaler)
     X_test_scaled = scaling(X_test, scaler, False)
-    random_forest = RandomForestClassifier(random_state=42, bootstrap= False, class_weight= 'balanced', criterion= 'entropy', max_depth= 160, max_features= 'sqrt', min_samples_leaf= 6, min_samples_split= 6, n_estimators= 1200)
+    random_forest = RandomForestClassifier(**rf_params)
     random_forest.fit(X_train_scaled, y_train)
     y_pred = random_forest.predict(X_test_scaled)
     accuracy = accuracy_score(y_test, y_pred)
@@ -26,19 +36,20 @@ def testing_accuracy(dataframe):
     print("Accuracy:", accuracy)
     print("Balanced Accuracy:", balanced_accuracy)
 
-testrun = testing_accuracy(final_df)
+#testrun = testing_accuracy(final_df, rf_parameters)
 
-def create_submission_randomforest(dataframe, newfilename):
-    """Trains a random forest model with all training data to create a file to use for submission in Kaggle."""
-    X_train = final_df.drop(columns=["target_feature"], axis=1)
+def create_submission_randomforest(dataframe, rf_params, newfilename):
+    """Trains a random forest model with all training data to create a csv file to use for submission in Kaggle.
+    This file has one column with Unique_ID, and one column with the predicted class 0 or 1."""
+    X_train = final_df.drop(columns=["target_feature", "SMILES_canonical"], axis=1) # only features for training
     y_train = final_df["target_feature"] 
     X_train_scaled = scaling(X_train, scaler)
-    X_train_scaled = pd.DataFrame(X_train_scaled, columns=X_train.columns, index=X_train.index)
-    random_forest = RandomForestClassifier(random_state=42, bootstrap= False, class_weight= 'balanced', criterion= 'entropy', max_depth= 160, max_features= 'sqrt', min_samples_leaf= 6, min_samples_split= 6, n_estimators= 1200)
+    X_train_scaled = pd.DataFrame(X_train_scaled, columns=X_train.columns, index=X_train.index) # keep feature names the same
+    random_forest = RandomForestClassifier(**rf_params)
     random_forest.fit(X_train_scaled, y_train)
     submission(random_forest, newfilename, scaler)
 
-#run = create_submission_randomforest(final_df, "predictions_output15.csv")
+run = create_submission_randomforest(final_df, rf_parameters, "predictions_output16.csv")
 
 
 
