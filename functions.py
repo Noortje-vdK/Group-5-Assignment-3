a1d5c22f6855acc0d8a6ccfa38831a21be3ca283
Koolwijk <n.a.v.d.koolwijk@student.tue.nl>
2025-01-05 16:51:04 +0100

    correlation

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 from rdkit import Chem
 import pandas as pd
+import numpy as np
 from rdkit.Chem import Descriptors
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
@@ -44,19 +45,34 @@ def calculate_descriptors(df):
     descriptors_df = pd.DataFrame(all_descriptors)
     return descriptors_df
 
+def remove_correlated_variables(data, threshold):
+    X = data.drop(columns=["SMILES_canonical"])
+    corr_matrix = X.drop(columns=["target_feature"]).corr()
+    upper_triangle = corr_matrix.where(np.triu(np.ones(corr_matrix.shape, dtype=bool), k=1))
+    to_drop = [col for col in upper_triangle.columns if any(upper_triangle[col] > threshold)]
+    return X.drop(columns=to_drop)
+
 def train_test_sets(X):
     """Splits dataframe X in training (0.8) and test (0.2) set."""
-    input = X.drop(columns=["SMILES_canonical", "target_feature"], axis=1)
+    input = X.drop(columns=["target_feature"], axis=1)
     output = X["target_feature"]
     X_train, X_test, y_train, y_test = train_test_split(input, output, test_size=0.2, random_state=42)
     return X_train, X_test, y_train, y_test
+
+scaler = StandardScaler()
+def scaling(data, scaler, fit = True):
+    if fit:
+        return scaler.fit_transform(data)
+    else:
+        return scaler.transform(data)
 
 def submission(model, newfile, scaler, testfile="test.csv"):
     """Creates a csv file that predicts the target feature for testfile"""
     new_data = data_processing(testfile, False)
     new_descriptors = calculate_descriptors(new_data)
-    new_descriptors.fillna(0)
-    new_descriptors_scaled = scaler.transform(new_descriptors)
+    new_descriptors = remove_correlated_variables(new_descriptors, 0.9)
+    new_descriptors.fillna(0, inplace=True)
+    new_descriptors_scaled = scaling(new_descriptors, scaler, False)
     predictions = model.predict(new_descriptors_scaled)
     new_data['target_feature'] = predictions
     new_data['target_feature'] = new_data['target_feature'].astype(str)
--- hyperparameter tuning.py
+++ hyperparameter tuning.py
@@ -4,18 +4,19 @@ import pandas as pd
 from sklearn.metrics import balanced_accuracy_score
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.model_selection import GridSearchCV
-from functions import data_processing, train_test_sets, calculate_descriptors
+from functions import data_processing, scaling, train_test_sets, remove_correlated_variables, calculate_descriptors
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
 
-def hyperparameters_randomsearch(data_path):
+def hyperparameters_randomsearch(filename):
     """
     Perform hyperparameter tuning for a RandomForestClassifier using RandomizedSearchCV.
     """
-    n_estimators = [int(x) for x in np.linspace(start=100, stop=1000, num=10)]
+    n_estimators = [int(x) for x in np.linspace(start=500, stop=1500, num=15)]
     max_features = ['log2', 'sqrt', 'auto']
-    max_depth = [int(x) for x in np.linspace(5, 100, num=10)]
+    max_depth = [int(x) for x in np.linspace(20, 400, num=12)]
     max_depth.append(None)
-    min_samples_split = [int(x) for x in np.linspace(start=2, stop=20, num=4)]
-    min_samples_leaf = [int(x) for x in np.linspace(start=2, stop=10, num=4)]
+    min_samples_split = [int(x) for x in np.linspace(start=2, stop=20, num=6)]
+    min_samples_leaf = [int(x) for x in np.linspace(start=1, stop=10, num=6)]
     bootstrap = [True, False]
     criterion = ["gini", 'entropy']
     class_weight = ['balanced', 'balanced_subsample']
@@ -41,17 +42,21 @@ def hyperparameters_randomsearch(data_path):
         random_state=42,
         n_jobs=-1)
 
-    training_data = data_processing(data_path, True)
+    training_data = data_processing(filename, True)
     train_descriptors = calculate_descriptors(training_data)
     final_df = pd.concat([training_data.reset_index(drop=True), train_descriptors], axis=1)
-    X_train, X_test, y_train, y_test = train_test_sets(final_df)
+    data = remove_correlated_variables(final_df, 0.9)
+    scaler = StandardScaler()
+    X_train, X_test, y_train, y_test = train_test_sets(data)
+    X_train_scaled = scaling(X_train, scaler)
+    X_test_scaled = scaling(X_test, scaler, False)
 
-    rf_random.fit(X_train, y_train)
+    rf_random.fit(X_train_scaled, y_train)
 
     best_params = rf_random.best_params_
 
     best_model = rf_random.best_estimator_
-    y_pred = best_model.predict(X_test)
+    y_pred = best_model.predict(X_test_scaled)
     balanced_accuracy = balanced_accuracy_score(y_test, y_pred)
 
     print(f"Best hyperparameters: {best_params}")
@@ -62,24 +67,28 @@ def hyperparameters_randomsearch(data_path):
 #best_params_random = hyperparameters_randomsearch("train.csv")
 #print(f"Best hyperparameters from random search: {best_params_random}")
 
-def hyperparameters_gridsearch(training_data):
+def hyperparameters_gridsearch(filename):
     """
     Perform hyperparameter tuning for a RandomForestClassifier using GridSearchCV.
     """
     param_grid = {
         'n_estimators': [1000, 1200, 1400],
-        'max_depth': [12, 14, 16, None],
+        'max_depth': [120, 160, 190, None],
         'max_features': ['sqrt', 'log2', 'auto'],
-        'min_samples_split': [7, 8, 9],
-        'min_samples_leaf': [3, 4, 5],
+        'min_samples_split': [6, 8, 10],
+        'min_samples_leaf': [2, 4, 6],
         'bootstrap': [True, False],
         'criterion' : ["gini", 'entropy'],
         'class_weight' : ['balanced', 'balanced_subsample']}
 
-    training_data = data_processing(training_data, True)
+    training_data = data_processing(filename, True)
     train_descriptors = calculate_descriptors(training_data)
     final_df = pd.concat([training_data.reset_index(drop=True), train_descriptors], axis=1)
-    X_train, X_test, y_train, y_test = train_test_sets(final_df)
+    data = remove_correlated_variables(final_df, 0.9)
+    scaler = StandardScaler()
+    X_train, X_test, y_train, y_test = train_test_sets(data)
+    X_train_scaled = scaling(X_train, scaler)
+    X_test_scaled = scaling(X_test, scaler, False)
 
     rf = RandomForestClassifier(random_state=42)
 
@@ -91,12 +100,12 @@ def hyperparameters_gridsearch(training_data):
         verbose=2,
         scoring='balanced_accuracy')
 
-    grid_search.fit(X_train, y_train)
+    grid_search.fit(X_train_scaled, y_train)
 
     best_params = grid_search.best_params_
     best_model = grid_search.best_estimator_
 
-    y_pred = best_model.predict(X_test)
+    y_pred = best_model.predict(X_test_scaled)
     balanced_accuracy = balanced_accuracy_score(y_test, y_pred)
 
     print(f"Best hyperparameters: {best_params}")
